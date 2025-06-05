a7ef782991e73c33d5a1b6c9301168ec29d16335
wliu080 <simonliu911@gmail.com>
2025-06-05 21:14:47 +0900

    add global level ja font

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document"
 
-export default function Document() {
+export default function Document({ __NEXT_DATA__ }) {
+    const lang = __NEXT_DATA__.locale || "en"
+
     return (
-        <Html lang="en">
+        <Html lang={lang}>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
