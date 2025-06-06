ab3f6e67f9331218445e44700f49dcd952a225f3
wliu080 <simonliu911@gmail.com>
2025-06-06 19:22:46 +0900

    include Open Sans JP

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,10 @@ export default function Document({ __NEXT_DATA__ }) {
                     href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
                     rel="stylesheet"
                 />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+                    rel="stylesheet"
+                />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <body>
