9690804ceead0469fc7ad5edc8ff9d317dac5644
Paul Kim <paulkim@designblooz.com>
2025-06-16 20:17:15 -0400

    upgrade typescript

--- components/common/OrderBook.tsx
+++ components/common/OrderBook.tsx
@@ -9,7 +9,7 @@ export default function OrderBook() {
     const { t, i18n } = useTranslation("common")
     const language = i18n.language
 
-    const orderRegionsMap = t("order.regions", { returnObjects: true }) as OrderRegionType[]
+    const orderRegionsMap: OrderRegionType[] = t("order.regions", { returnObjects: true }) as OrderRegionType[]
 
     function ENSection() {
         return (
