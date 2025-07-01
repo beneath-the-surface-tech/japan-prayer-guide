105da95ee72ca572ad3466a81d1d316ef52499be
Paul Kim <paulkim@designblooz.com>
2025-06-30 20:07:49 -0400

    update mosaic

--- components/topic/Timeline/EventContent.tsx
+++ components/topic/Timeline/EventContent.tsx
@@ -3,9 +3,12 @@ import { motion } from "motion/react"
 import { Image } from "react-bootstrap"
 import styles from "./Timeline.module.scss"
 import { PhotosWrapper } from "@/components/GalleryComponents/PhotosWrapper/PhotosWrapper"
+import { Mosaic } from "@/components/GalleryComponents/Mosaic/Mosaic"
+import type { TimelineEvent, TimelinePhoto } from "@/pages/topics/[topicPage]"
+import { Box } from "@mui/material"
 
 interface EventContentProps {
-    activeEvent: any // TimelineEvent, but avoid circular import for now
+    activeEvent: TimelineEvent // TimelineEvent, but avoid circular import for now
     textColor: string
 }
 
@@ -23,7 +26,7 @@ const EventContent: React.FC<EventContentProps> = ({ activeEvent, textColor }) =
                         className={styles.eventImage}
                     />
                 )}
-                {activeEvent.galleryType !== "single" && (
+                {activeEvent.galleryType !== "single" && activeEvent.galleryType !== "mosaic" && (
                     <PhotosWrapper
                         type={activeEvent.galleryType}
                         images={activeEvent.photos.map((photo: any) => ({
@@ -40,9 +43,47 @@ const EventContent: React.FC<EventContentProps> = ({ activeEvent, textColor }) =
                         alwaysDesktop={true}
                     />
                 )}
+                {activeEvent.galleryType === "mosaic" && <CustomMosaicComponent activeEvent={activeEvent} />}
             </div>
         )}
     </motion.div>
 )
+
+const CustomMosaicComponent = ({ activeEvent }: { activeEvent: TimelineEvent }) => {
+    const imagesPerRow = 9
+    const splitPhotos = activeEvent.photos?.reduce((acc: TimelinePhoto[][], photo: TimelinePhoto, index: number) => {
+        if (index % imagesPerRow === 0) {
+            acc.push([photo])
+        } else {
+            acc[acc.length - 1].push(photo)
+        }
+        return acc
+    }, [])
+
+    console.log(splitPhotos)
+
+    return (
+        <>
+            {splitPhotos?.map((photo: TimelinePhoto[], index: number) => (
+                <Box mt="-55px" position="relative" key={index}>
+                    <Mosaic
+                        images={photo.map((photo: TimelinePhoto) => ({
+                            src: photo.src,
+                            title: photo.title || "",
+                            alt: photo.alt || "",
+                        }))}
+                        uncropped={photo.map((photo: TimelinePhoto) => ({
+                            src: photo.src,
+                            title: photo.title || "",
+                            alt: photo.alt || "",
+                        }))}
+                        blocks={[5, 4, 1, 8]}
+                        subTitle=""
+                    />
+                </Box>
+            ))}
+        </>
+    )
+}
 
 export default EventContent
