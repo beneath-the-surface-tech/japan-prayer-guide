0701ad64b851f11960c9de51e441ecd376c9744a
wliu080 <simonliu911@gmail.com>
2025-08-18 09:06:40 +0900

    update metadata for topic pages

--- pages/topics/[topicPage].tsx
+++ pages/topics/[topicPage].tsx
@@ -73,7 +73,6 @@ export default function TopicPage({ localeRef }: { localeRef: string }) {
     const navTabs: Tab[] = topicCommon("nav", { returnObjects: true }) as Tab[]
 
     const title: string = t("title")
-    const metaDescription: string = t("metaDescription")
     const galleryLabel: string = topicCommon("galleryLabel")
     const factsLabel: string = topicCommon("factsLabel")
     const galleryClickInstructions: string = topicCommon("galleryClickInstructions")
@@ -101,7 +100,7 @@ export default function TopicPage({ localeRef }: { localeRef: string }) {
 
     return (
         <>
-            <AppHeader title={title} description={metaDescription} pageType="article" image={heroPhoto} />
+            <AppHeader title={title} description={title} pageType="article" image={heroPhoto} />
             <main role="main">
                 {/* Header Component */}
                 <ToggleHeader hideShadow={true} />
